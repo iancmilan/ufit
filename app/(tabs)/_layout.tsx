f1c5f3b3173cc11e6e675fe40346ba5ac7d9bb59
ian <iancmilan@unochapeco.edu.br>
2023-08-07 22:25:59 -0300

    adicionando icones 🍴

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,54 @@
 import { Tabs } from 'expo-router'
 
+import {
+  Ionicons,
+  FontAwesome5,
+  MaterialCommunityIcons,
+} from '@expo/vector-icons'
+
 export default function TabRoutesLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="community" options={{ title: 'Community' }} />
-      <Tabs.Screen name="workout" options={{ title: 'Workout' }} />
-      <Tabs.Screen name="meals" options={{ title: 'Meals' }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="community"
+        options={{
+          title: 'Community',
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome5 name="globe-americas" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="workout"
+        options={{
+          title: 'Workout',
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="barbell" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="meals"
+        options={{
+          title: 'Meals',
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons
+              name="silverware-fork-knife"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   )
 }
