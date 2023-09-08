3d8cad4814d41f4d99154a22d1642a046a5cf5f5
ian <iancmilan@unochapeco.edu.br>
2023-09-08 15:57:05 -0300

    adicionando chamada a api 📞

--- app/(tabs)/(workouts)/workouts.tsx
+++ app/(tabs)/(workouts)/workouts.tsx
@@ -1,7 +1,33 @@
 import { View } from 'react-native'
 import Workout from '../../components/Workout'
+import { api } from '../../services/api'
+import { useEffect, useState } from 'react'
+
+interface Exercise {
+  id: number
+  name: string
+}
+
+interface Workout {
+  id: string
+  title: string
+  exercises: Exercise[]
+}
 
 export default function Workouts() {
+  const [workouts, setWorkouts] = useState<Workout[]>([])
+
+  async function loadWorkouts() {
+    const response = await api.get('/workouts?_embed=exercises')
+    setWorkouts(response.data)
+    console.log(workouts)
+  }
+
+  useEffect(() => {
+    loadWorkouts()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <View className="flex-1 items-center bg-[#11141B] px-6 py-6">
       <Workout
